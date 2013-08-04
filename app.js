cd66908a7be1316cbfdf177dced2db149445df3d
Erik Johnson <erik@erikaugust.com>
2013-08-04 06:04:55 -0400

    Added ability to select options dynamically

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var app = angular.module("flangExample", ["flang"]);
 
 app.controller("flangCtrl", function ($scope, $http) { 
-	$scope.example_data = 
+	$scope.example = Object();
+    $scope.example.data = 
 	[
 		{ label: "Series1",  data: 10},
 		{ label: "Series2",  data: 30},
@@ -11,5 +12,35 @@ app.controller("flangCtrl", function ($scope, $http) {
 		{ label: "Series6",  data: 110}
 	];
 
+    $scope.example.options = 
+    {
+            colors: ["#38a", "#222", "#666"],
+            series: {
+                pie: {
+                    show: true,
+                    label: {
+                        show: false,
+                        formatter: function(label, series){
+                            return '<div style="font-size:11px;text-align:center;padding:4px;color:white;">'+label+'<br/>'+series.percent+'%</div>';
+                        },
+                        threshold: 0.1
+                    }
+                }
+            },
+            legend: {
+                show: true,
+                noColumns: 1, // number of colums in legend table
+                labelFormatter: null, // fn: string -> string
+                labelBoxBorderColor: "#888", // border color for the little label boxes
+                container: null, // container (as jQuery object) to put legend in, null means default on top of graph
+                position: "ne", // position of default legend container within plot
+                margin: [5, 10], // distance from grid edge to default legend container within plot
+                backgroundOpacity: 0 // set to 0 to avoid background
+            },
+            grid: {
+                hoverable: false,
+                clickable: false
+            },
+    }
 
 });
